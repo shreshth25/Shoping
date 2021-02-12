f2e70b2d165bb74471823ea55f9959a17443a4d4
shreshthbansal <shreshth.bansal@unthinkable.co>
2021-02-13 00:38:37 +0530

    fourth commit

---
+++ resources/views/customer/common/scripts.blade.php
@@ -0,0 +1,15 @@
+<script src="{{asset('customer/js/jquery-3.2.1.min.js')}}"></script>
+<script src="{{asset('customer/js/popper.min.js')}}"></script>
+<script src="{{asset('customer/js/bootstrap.min.js')}}"></script>
+<!-- ALL PLUGINS -->
+<script src="{{asset('customer/js/jquery.superslides.min.js')}}"></script>
+<script src="{{asset('customer/js/bootstrap-select.js')}}"></script>
+<script src="js/inewsticker.js"></script>
+<script src="js/bootsnav.js."></script>
+<script src="js/images-loded.min.js"></script>
+<script src="js/isotope.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/baguetteBox.min.js"></script>
+<script src="js/form-validator.min.js"></script>
+<script src="js/contact-form-script.js"></script>
+<script src="js/custom.js"></script>
